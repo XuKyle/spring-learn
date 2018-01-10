fede182a235369592b75ac2540ca1c7099a40181
kyle <Kyle.Xu@nielsenccdata.com>
2018-01-10 21:13:27 +0800

    1.0110同步

--- src/main/java/com/kylexu/springlearn/ch1/KnightMain.java
+++ src/main/java/com/kylexu/springlearn/ch1/KnightMain.java
@@ -4,7 +4,8 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class KnightMain {
     public static void main(String[] args) {
-        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("ch1/knight.xml");
+//        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("ch1/knight.xml");
+        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("ch1/minstrel.xml");
         Knight knight = context.getBean(Knight.class);
         knight.embarkOnQuest();
         context.close();
